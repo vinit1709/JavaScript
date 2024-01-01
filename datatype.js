b5fa8aeec2fbb0e1ee96d6d7e83be5887f1092f8
Vinit Patel <128422482+vinit1709@users.noreply.github.com>
2024-01-01 19:27:30 +0530

    Datatypes

---
+++ datatype.js
@@ -0,0 +1,13 @@
+"use strict";  //treat all JS code as newer version.
+
+// alert("hello")   // we are using nodejs, not browser!!
+
+// code readblitity should be heigh
+console.log(3 + 3); console.log("Vinit")   // don't write this type code!!
+
+let name = "Vinit"          // String  => ""
+let age = 20                // Number  => 2 to power 53
+let isLogeedIn = false      // boolean => true/false
+let state = null            // null    => standalone value
+let city;                 // undefined => 
+                            // symbol  =>
