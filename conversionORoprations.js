fe230c8299efde045b62aa19e3d62cf9bdb75f7a
Vinit Patel <128422482+vinit1709@users.noreply.github.com>
2024-01-06 21:30:39 +0530

    Conversion of Datatype

---
+++ conversionORoprations.js
@@ -0,0 +1,23 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 or false => 0
+// "" => false
+// "Vinit" => true
+let isLogeedIn = "Vinit"
+
+let booleanIsLoggedIn = Boolean(isLogeedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
