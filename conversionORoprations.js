1f2e141b24e324feae50e2b9c5c16ff77e51a20e
Vinit Patel <128422482+vinit1709@users.noreply.github.com>
2024-01-06 21:50:53 +0530

    Operations of Datatype

--- conversionORoprations.js
+++ conversionORoprations.js
@@ -1,11 +1,11 @@
 let score = "33"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -15,9 +15,47 @@ console.log(valueInNumber);
 let isLogeedIn = "Vinit"
 
 let booleanIsLoggedIn = Boolean(isLogeedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 let someNumber = 33
+
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// **************************** Operations ********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello "
+let str2 = "Vinit"
+
+let str3 = str1 + str2
+// console.log(str3);
+//                                 // Ans
+// console.log("1" + 2);           // 12
+// console.log(1 + "2")            // 12
+// console.log("1" + 2 + 2);       // 122
+// console.log(1 + 2 + "2");       // 32
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
