8807afeb265db5dbde6721dece84c25b358dad33
Vinit Patel <128422482+vinit1709@users.noreply.github.com>
2024-01-07 12:46:51 +0530

    Strings

---
+++ string.js
@@ -0,0 +1,34 @@
+const name = "vinit"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+// console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`);
+
+const gameName = new String("Rdx_Vinit_Com")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-7, 4)
+// console.log(anotherString);
+
+const newStringOne = "     vinit      " 
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://vinit.com/vinit%20patel"
+
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('vinit'));
+
+console.log(gameName.split('_'));
